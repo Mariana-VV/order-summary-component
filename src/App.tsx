050102c12039fe52888c48aa73a553c45e2dd0b7
Mariana Trinko <marjanavv@gmail.com>
2025-09-27 10:06:14 +0200

    created project

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <article className="card">
+        <div className="card__image">
+          <img src="../assets/images/hero.png" alt="hero image" />
+        </div>
+        <div className="card__content">
+          <h1 className="card__title">Order Summary</h1>
+          <p className="card__description">
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+          <div className="card__box">
+            <div className="card__info">
+              <div className="card__plan">
+                <div className="card__icon">
+                  <img src="../assets/images/icon.svg" alt="icon" />
+                </div>
+                <div className="card__paragraphs">
+                  <p>Annual Plan</p>
+                  <p>$59.99/year</p>
+                </div>
+              </div>
+            </div>
+
+            <a href="#" className="card__change">
+              Change
+            </a>
+          </div>
+
+          <button className="card__btn">Proceed to Payment</button>
+          <button className="card__cancel">Cancel Order</button>
+        </div>
+      </article>
+    </>
+  );
+}
+
+export default App;
